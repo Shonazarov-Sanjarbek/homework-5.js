f16ea0518c1891fd815cc6a1529ee59a0242e07e
Shonazarov-Sanjarbek <shonazarovsanjarbek71@gmail.com>
2024-06-10 03:46:44 +0500

    js daki==gi masalalar

---
+++ js/main.js
@@ -0,0 +1,150 @@
+
+// {
+        // 1 - masala
+//     function perimetr() {
+//       let a =  +prompt("kvadratning a tomonini kiriting va perimetri aniqlanib console da chiqarilsin")
+//         return 4 * a
+    
+//     }
+    
+    // console.log(`P = ${perimetr()}`);
+
+// }
+
+
+// {
+//         2-masala
+//     function yuza() {
+//         let a = +prompt("Kvadratning a tomonini kiriting va uning yuzasi topilsin, chiqqan natija consoleda paydo bo'lsin")
+//         return a * a
+//     }
+    
+//     console.log(`S = ${yuza()}`);
+
+// }
+
+// {
+//     3 - masala
+//         alert("To'g'ri to'rtburchakning tomonlari a va b kiritilganda uning yuzasi va perimetri topilib natijasi consoleda chiqsin")
+//         let a = +prompt("a sonini kiriting")
+//         let b = +prompt("b sonini kiriting")
+//         function perimetr() {
+//             function yuza() {   
+//                 return a * b
+//             }
+//             console.log(`S = ${yuza()}`);
+            
+//             return 2 * ( a + b)
+//         }
+//             console.log(`P = ${perimetr()}`);
+
+// }
+    
+// {
+//     4 - masala
+//     alert("Aylananing diametri kiritilganda uning uzunligi topilsin")
+//     let d = +prompt("aylananing diametrini kiriting")
+//     let p = 3.14
+//     function uzunligi() {
+//         return p * d
+//     }
+//     console.log(`L = ${uzunligi()}`);
+// }
+
+// {
+//     // 5 - masala
+//     alert("Kubning yon tomoni (a) kiritilganda uning hajmi va to'la sirti topilsin")
+//     let a = +prompt("a tomonini kiriting")
+//     function hajmi() {
+//         return a * a * a
+//     }
+//     console.log(`V = ${hajmi()}`);
+//     function tolaSirt() {
+//         return 6 * a * a
+//     }
+//     console.log(`S = ${tolaSirt()}`);
+// }
+    
+// {
+//     // 6 - masala
+//     alert("paralelepipedning tomonlarini kiritganda uning hajmi va to'la sirti topilsin")
+//     let a = +prompt("a tomonini kiriting")
+//     let b = +prompt("b tomonini kiriting")
+//     let c = +prompt("c tomonini kiriting")
+//     function hajmi() {
+//         return a * b * c
+//     }
+//     console.log(` V = ${hajmi()}`);
+//     function tolaSirt() {
+//         return 2 * ( a * b + b * c + a * c)
+//     }
+//     console.log(` S = ${tolaSirt()}`);
+// }
+
+// {
+//     // 7 - masala
+//     alert(" doiraning radiusini kiritganda uning uzunligi(L) va yuzasi(S) aniqlansin")
+//     let p = 3.14
+//     let r = +prompt("doira radiusini kiriting (r)")
+//     function L() {
+//         return 2 * p * r
+//     }
+//     console.log(` L = ${L()}`);
+//     function S() {
+//         return p * r * r
+//     }
+//     console.log(` S = ${S()}`);
+// }
+
+// {
+//     // 8 - masala 
+//     alert("ikkita son kiritganda ularni o'rta arifmetigi topilsin")
+//     let a = +prompt("a sonini kiriting")
+//     let b = +prompt("b sonini kiriting")
+//     function ortaArifmetik() {
+//         return (a + b) / 2
+//     }
+//     console.log(`o'rta arifmetigi = ${ortaArifmetik()}`);
+// }
+
+// {
+//     // 9 - masala
+//     alert(" Ikkita manfiy bo'lmagan sonni kiritganda ularni o'rta geometrigi topilsin")
+//     let a = +prompt("a sonini kiriting")
+//     let b = +prompt("b sonini kiriting")
+//     function ortageometrik() {
+//         if (a >= 0 & b >= 0) {
+//             return Math.sqrt(a * b) 
+//         }
+//     }
+//     console.log(ortageometrik());
+// }
+
+// {
+//     // 10 - masala
+//     alert("0 ta teng bo'lmagan ikkita a va b sonlari kiritilganda ularning yig'indisi va har birining kvadrati  aniqlansin")
+
+//     let a = +prompt("a sonini kiriting")
+//     let b = +prompt("b sonini kiriting")
+//     function yigindi() {
+//         if (a !== 0 & b !== 0) {
+//             return a + b
+//         }
+//     }
+//     console.log(`yig'indi = ${yigindi()}`);
+
+//     function aKvadrati() {
+//         if (a !== 0) {
+//             return a * a
+//         }
+//     }
+//     console.log(`a ning kvadrati = ${aKvadrati()}`);
+
+//     function bKvadrati() {
+//         if (b !== 0) {
+//             return b * b
+//         }
+//     }
+//     console.log(`b ning kvadrati = ${bKvadrati()}`);
+
+// }
